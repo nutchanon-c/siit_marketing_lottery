050074abf6c377f04bfc5f47e9e9fd82fe846655
Nont <nutchanon.char@gmail.com>
2022-03-24 13:57:56 +0700

    added complementary color

--- lottery.py
+++ lottery.py
@@ -17,7 +17,18 @@ with open(currentDir + "/config.json", 'r', encoding="utf8") as f:
 
 
 
+def complementaryColor(my_hex):
+    """Returns complementary RGB color
 
+    Example:
+    >>>complementaryColor('FFFFFF')
+    '000000'
+    """
+    if my_hex[0] == '#':
+        my_hex = my_hex[1:]
+    rgb = (my_hex[0:2], my_hex[2:4], my_hex[4:6])
+    comp = ['%02X' % (255 - int(a, 16)) for a in rgb]
+    return ''.join(comp)
 
 
 
@@ -71,7 +82,7 @@ def main():
             
         ],
         [
-            sg.Text('Made by Nutchanon Charnwutiwong',expand_x=True, justification='r', background_color=colors['backgroundColor'],text_color="#000000")
+            sg.Text('Made by Nutchanon Charnwutiwong',expand_x=True, justification='r', background_color=colors['backgroundColor'],text_color="#"+complementaryColor(backgroundColor[1:]))
         ]
 
         
